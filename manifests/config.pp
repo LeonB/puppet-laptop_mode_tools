cd3a9387603896d59dd4b6f27698efc67520905d
Leon Bogaert <leon@tim-online.nl>
2013-09-11 11:21:31 +0200

    Added Ubuntu recomendation: don't know if it's still up to date

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,15 @@
 class laptop_mode_tools::config {
 
-    ## do package before config
-    Class['laptop_mode_tools::config'] -> Class['laptop_mode_tools::package']
+  ## do package before config
+  Class['laptop_mode_tools::config'] -> Class['laptop_mode_tools::package']
+
+  # See https://help.ubuntu.com/community/PowerManagement/ReducedPower
+  file { '/etc/pm/sleep.d/10-laptop-mode-tools':
+    ensure => $laptop_mode_tools::ensure,
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/laptop_mode_tools/10-laptop-mode-tools',
+    mode   => 0755, #rwx,rxmrx
+  }
 
 }
